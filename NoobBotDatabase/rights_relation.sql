9dbb0ea4eb766a1c6a3fe0451a5b1c2bcd2ccc28
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-02-11 12:45:55 +0100

    Database to Azure

--- NoobBotDatabase/rights_relation.sql
+++ NoobBotDatabase/rights_relation.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[rights_relation]
 (
+	[id] int not null identity(0,1),
 	[is_group] bit not null default 0,
 	[reference] int not null,
 	[right_id] varchar(max) not null,
-	[power] tinyint not null default 0
+	[power] tinyint not null default 0, 
+    CONSTRAINT [PK_rights_relation] PRIMARY KEY ([id])
 	
 
 )
